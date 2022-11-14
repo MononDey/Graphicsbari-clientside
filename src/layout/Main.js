1caacd3c6d71b9695403ec0a82a25ba5297d4b74
Monon Kanti Dey <mononkanti@gmail.com>
2022-11-14 21:22:50 +0600

    5

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Footer from '../pages/Shared/Footer/Footer';
+
 import Header from '../pages/Shared/Header/Header';
 
 const Main = () => {
